855969c5d645a5776c5b18ce56eafe6200e27bbd
punkushal <spycymagar@gmail.com>
2023-10-18 09:35:05 +0545

    Using Device Camera To Take Picture

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,23 +11,50 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+    if (pickedImage == null) {
+      return;
+    }
+    //to see picture after clicked by camera we have to use setState()
+    setState(() {
+      //To convert XFile to File we use File(pickedImage.path)
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border:
-            Border.all(width: 1, color: Theme.of(context).colorScheme.primary),
-      ),
-      height: 250,
-      width: double.infinity,
-      //this alignment centers child element horizontally as well as vertically
-      alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: _takePicture,
-        icon: const Icon(Icons.camera),
-        label: Text('Take Picture'),
-      ),
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera),
+      label: Text('Take Picture'),
     );
+    if (_selectedImage != null) {
+      //to go again to device camera and take picture and change it i used GestureDetector
+      //where there is property onTap to again call the _takePicture function
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+    return Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+              width: 1, color: Theme.of(context).colorScheme.primary),
+        ),
+        height: 250,
+        width: double.infinity,
+        //this alignment centers child element horizontally as well as vertically
+        alignment: Alignment.center,
+        child: content);
   }
 }
